572c1c55cd6cf130e0624d9d5b605be771ea855d
DiMario <51769035+LightNuc@users.noreply.github.com>
2026-08-24 15:18:17 -0300

    Delete codigo directory

--- codigo/Fundamentos.java
+++
@@ -1,5 +0,0 @@
-public class Fundamentos {
-    public static void main(String[] args) {
-        System.out.println("Ola Guilherme");
-    }
-}
--- codigo/Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-  public static void main(String[] args){
-    // a linha abaixo exibe informação na tela
-    System.out.println("Ola!!! ADS turma A");
-    System.out.println("Quero camarão");
-   }
-
-
-}
